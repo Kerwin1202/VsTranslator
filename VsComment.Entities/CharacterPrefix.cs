6f4d10b4a9c61c567ae94cba5348e4288fd8350d
kerwin <me@zhanghuanglong.com>
2017-03-06 18:37:59 +0800

    comment

---
+++ VsComment.Entities/CharacterPrefix.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace VsComment.Entities
+{
+    /// <summary>
+    /// Character Prefix, use this to set comment different color
+    /// </summary>
+    public class CharacterPrefix
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Character
+        /// <example>!@</example>
+        /// </summary>
+        public string Character { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The description for character
+        /// </summary>
+        public string Description { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The color for comment with the corresponding character
+        /// </summary>
+        public string Color { get; set; } = "#000000";
+
+        /// <summary>
+        /// The time when this character created
+        /// </summary>
+        public DateTime CreateTime { get; set; } = DateTime.Now;
+
+        /// <summary>
+        /// The count of quoted for this character
+        /// </summary>
+        public int QuotesCount { get; set; } = 0;
+    }
+}
---
+++ VsComment.Entities/Comment.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VsComment.Entities
+{
+    public class Comment
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The character prefix of the comment
+        /// </summary>
+        public int CharacterPrefixId { get; set; }
+
+        /// <summary>
+        /// The keyword of this comment
+        /// </summary>
+        public string Keyword { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The content of this comment
+        /// </summary>
+        public string CommentContent { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The time when this comment was created
+        /// </summary>
+        public DateTime CreateTime { get; set; } = DateTime.Now;
+
+        /// <summary>
+        /// The file info of quote this comment
+        /// </summary>
+        public List<ProjectFileInfo> QuotesFileInfo { get; set; } = new List<ProjectFileInfo>();
+    }
+}
