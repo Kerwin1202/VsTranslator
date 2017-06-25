9e848e101491332563859462758867d93736d217
Kerwin <me@zhanghuanglong.com>
2017-06-25 11:30:59 +0800

    Only retained VsTranslate

--- Codes/TranslateService/Service.svc.cs
+++ Codes/TranslateService/Service.svc.cs
@@ -12,6 +12,9 @@ namespace TranslateService
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service : IService
     {
-       
+        public string GetData(int value)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
