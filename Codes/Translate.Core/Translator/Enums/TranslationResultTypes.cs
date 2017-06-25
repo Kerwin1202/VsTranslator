c5ed737065191a3f3c73df523ac5356788e10a2b
Kerwin <me@zhanghuanglong.com>
2017-06-25 11:37:19 +0800

    Only retained VsComment

--- Codes/Translate.Core/Translator/Enums/TranslationResultTypes.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Translate.Core.Translator.Enums
-{
-    public enum TranslationResultTypes
-    {
-        Failed = 0,
-        Successed = 1
-    }
-}
--- Codes/VsBackground/Settings/Settings.cs
+++
@@ -1,9 +0,0 @@
-﻿using VsBackground.Grids;
-
-namespace VsBackground.Settings
-{
-    public static class Settings
-    {
-        public static SettingPageGrid SettingPage { get; set; }
-    }
-}
